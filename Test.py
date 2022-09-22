c473dffa6539e31ed2be48d8cc23f35735ef99e4
Janan <71359987+Jananzzzz@users.noreply.github.com>
2022-09-22 11:06:11 +0800

    update

--- Test.py
+++ Test.py
@@ -2,6 +2,43 @@
 
 
 
+
+
+
+
+
+
+
+
+"""
+x = int(2.3)
+print(x)
+while x:
+    print(1)
+    x = x - 1
+while int(2.3):                       # return can only used within a function
+"""
+
+
+# global variable
+"""
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+print("Python is: "+ x)
+"""
+
+
+'''
+x = str(3)             # x will be '3'
+y = int(3)             # y will be 3
+z = float(3)           # z will be 3.0
+print(type(z))
+'''
+
 # test input
 '''
 lst = []
