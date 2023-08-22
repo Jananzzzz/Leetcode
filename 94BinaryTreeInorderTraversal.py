7243705019aa3d51adc5fd2a390dd444cddab742
jananzzzz <bangzhubieren18@gmail.com>
2023-08-22 19:04:51 +0800

    restart

--- 94BinaryTreeInorderTraversal.py
+++
@@ -1,9 +0,0 @@
-def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    res = []
-    def inOrder(root):
-        if(root):
-            inOrder(root.left)
-            res.append(root.val)
-            inOrder(root.right)
-    inOrder(root) 
-    return res    
--- Algollection/Match.py
+++
@@ -1,7 +0,0 @@
-def match(a, b):
-    if not a: return not b         # while a != b, return true; a == b == 0, return true
-    first_match = bool(b) and a[0] in {b[0], '.'}
-    return first_match and match(a[1:], b[1:])
-
-    
-    # '*' Matches zero or more of the preceding element.
