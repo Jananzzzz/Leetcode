a540860e23889bbc939dc58a46117108adc2be8d
jananzzzz <bangzhubieren18@gmail.com>
2023-09-13 17:41:42 +0800

    add

---
+++ count.sh
@@ -0,0 +1,9 @@
+
+
+echo "hard:         $(find ./hard -type f | wc -l)"
+echo "medium:       $(find ./medium -type f | wc -l)"
+echo "easy:         $(find ./easy -type f | wc -l)"
+echo "playground:   $(find ./playground -type f | wc -l)"
+echo "demo:         $(find ./demo -type f | wc -l)"
+echo "test:         $(find ./test -type f | wc -l)"
+
